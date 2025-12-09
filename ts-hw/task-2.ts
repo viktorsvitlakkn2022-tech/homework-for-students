f515666f04336131f5aa3b80a94ef28511e8384c
viktorsvitlakkn2022-tech <viktor.svitlak.kn.2022@lpnu.ua>
2025-12-09 20:38:42 +0200

    Update task-2.ts

--- ts-hw/task-2.ts
+++ ts-hw/task-2.ts
@@ -1,11 +1,20 @@
 /*
     Обов'язкові й необов'язкові поля:
     Створи інтерфейс Product з ключами:
-    title (string), price (number), description (string, необов'язковий) 
+    title (string), price (number), description (string, необов'язковий)
     та призначити його тип об'єкту product.
 */
 
-const product = {
+// Створення інтерфейсу Product
+interface Product {
+    title: string;
+    price: number;
+    description?: string;
+}
+
+// Призначення інтерфейсу об'єкту product
+const product: Product = {
     title: "Телефон",
     price: 1000
+   
 };
