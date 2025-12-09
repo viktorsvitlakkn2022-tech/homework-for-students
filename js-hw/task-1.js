a214991249d319e09ba2be1d792f31f09728b667
viktorsvitlakkn2022-tech <viktor.svitlak.kn.2022@lpnu.ua>
2025-12-09 20:16:45 +0200

    Update task-1.js

--- js-hw/task-1.js
+++ js-hw/task-1.js
@@ -3,7 +3,26 @@
     Створити об'єкт з ключами name, age, 
     а також вкладений об'єкт address з ключами city і street.
 */
-
 const person = {
-    
+    name: "Олена",
+    age: 30,
+    address: {
+        city: "Київ",
+        street: "Хрещатик"
+    }
 };
+
+// 1. Доступ до простих властивостей (name, age)
+const personName = person.name;
+const personAge = person.age;
+
+// 2. Доступ до вкладених властивостей (city, street через address)
+const personCity = person.address.city;
+const personStreet = person.address.street;
+
+
+console.log('Імя:', personName);
+console.log('Вік:', personAge);
+console.log('Місто:', personCity);
+console.log('Вулиця:', personStreet);
+
