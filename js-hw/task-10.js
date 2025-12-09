c5879435f8b40ec67f09748a9f607afb829d5549
viktorsvitlakkn2022-tech <viktor.svitlak.kn.2022@lpnu.ua>
2025-12-09 20:30:24 +0200

    Update task-10.js

--- js-hw/task-10.js
+++ js-hw/task-10.js
@@ -1,6 +1,6 @@
 /*
     3. Простий Promise:
-    Є число x. 
+    Є число x.
     Якщо воно більше 10 — Promise має повернути resolve("Більше 10").
     Якщо менше або дорівнює 10 — reject("Менше або дорівнює 10").
 */
@@ -8,7 +8,14 @@
 const x = 12;
 
 const checkNumber = new Promise((resolve, reject) => {
-    // ...
+    // Вставте сюди:
+    if (x > 10) {
+        // Якщо умова виконується, викликаємо resolve()
+        resolve("Більше 10");
+    } else {
+        // Якщо умова НЕ виконується, викликаємо reject()
+        reject("Менше або дорівнює 10");
+    }
 });
 
 checkNumber
@@ -17,4 +24,4 @@ checkNumber
     })
     .catch((error) => {
         console.log("Помилка:", error);
-    }); 
+    });
